f76bb198bdfbbaece897ce6869537cb949ba8e2f
Callum Locke <callum.locke@gmail.com>
2015-05-04 22:17:08 +0100

    use browser-resolve-noio

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ module.exports = function (settings) {
     var path = require('path');
     var browserify = require('browserify');
     var streamifier = require('streamifier');
-    var async = require('async');
-    var browserResolve = require('browser-resolve');
+    var browserResolve = require('browser-resolve-noio');
     var Promise = require('bluebird');
     var cwd = process.cwd();
 
@@ -22,7 +21,7 @@ module.exports = function (settings) {
 
         return function () {
             if (!promise) {
-                var requireResolve = require('browser-resolve/node_modules/resolve');
+                var requireResolve = require('browser-resolve-noio/node_modules/resolve');
                 promise = Promise.all(transforms.map(function (transform) {
                     // return a promise to load this particular transform module
                     return new Promise(function (resolve, reject) {
@@ -71,7 +70,6 @@ module.exports = function (settings) {
         console.log.apply(null, args);
     };
 
-
     // cache all deps whenever they appear
     var packageCache = {};
     var cache = {}; // will contain dep objects, which already have transforms applied
@@ -84,10 +82,11 @@ module.exports = function (settings) {
 
         log('cache keys:\n', Object.keys(cache));
 
-        // Delete the trigger file(s) from the cache
+        // delete the trigger file(s) from the cache
         if (triggers) {
             triggers.forEach(function (trigger) {
-                trigger = path.resolve(self.sourceDir, trigger); // todo: do this with exhibit.resolveSourcePath, which should handle bower_components etc (and Source objects should use this too; move the logic out of Source and into Exhibit).
+                // console.log('TRIGGER', self.sourceDir);
+                trigger = path.resolve(self.sourceDir, trigger); // todo: using exhibit-provided method?
 
                 if (cache[trigger]) {
                     log('AM PURGING', trigger);
@@ -100,22 +99,22 @@ module.exports = function (settings) {
         }
 
         // make a phoney absolute path
-        var relPath = path.join(self.sourceDir, file.path);
-        var numDirectoryParts = process.cwd().split(path.sep).length;
-        var fakeBase = '';
-        for (var i = 1; i < numDirectoryParts; i++) {
-            fakeBase = path.join(fakeBase, '_');
-        }
-        fakeBase = path.join('/_fake_', fakeBase); // maybe do x:\ on windows?
-        var fullPath = path.join(fakeBase, self.sourceDir, file.path);
+        // var relPath = path.join(self.sourceDir, file.path);
+        // var numDirectoryParts = process.cwd().split(path.sep).length;
+        // var fakeBase = '';
+        // for (var i = 1; i < numDirectoryParts; i++) {
+        //     fakeBase = path.join(fakeBase, '_');
+        // }
+        // fakeBase = path.join('/_fake_', fakeBase); // maybe do x:\ on windows?
+        // var fullPath = path.join(fakeBase, self.sourceDir, file.path);
 
-        // ALTERNATIVE trying out using a real one...
-        fullPath = path.resolve(self.sourceDir, file.path);
+        var fullPath = path.resolve(self.sourceDir, file.path);
 
         var stream = streamifier.createReadStream(file.contents);
 
-        // just hang the file path on the stream object - this is undocumented but seems to be how watchify does it.
+        // just hang the file path on the stream object - this is undocumented but seems to be how watchify does it
         stream.file = fullPath; // https://github.com/substack/node-browserify/issues/816
+        // stream.file = relPath;
 
         var b = browserify({
             extensions: settings.extensions,
@@ -138,22 +137,23 @@ module.exports = function (settings) {
 
                 var options = extend(parent, {
                     isFile: function (filePath, cb) {
+                        log('ISFILECALLED', filePath);
                         // make it back into a relative path
-                        filePath = path.relative(fakeBase, filePath);
+                        // filePath = path.relative(fakeBase, filePath);
                         self.isFile(filePath, function (err, answer) {
 	                        log('ISFILE', filePath, err, answer);
                         	cb(err, answer);
                         });
                     },
                     readFile: function (filePath, encoding, cb) {
-                        filePath = path.relative(fakeBase, filePath);
-                        log('READFILE', filePath);
+                        log('READFILECALLED', filePath);
+                        // filePath = path.relative(fakeBase, filePath);
                         self.readFile(filePath, encoding, cb);
                     }
                 });
 
                 browserResolve(id, options, function (err, path) {
-                    if (err) console.log('WARNING did not resolve', id);
+                    // if (err) console.log('WARNING did not resolve', id, err);
 
                     cb(err, path);
                 });
@@ -182,22 +182,54 @@ module.exports = function (settings) {
             b.add(stream);
 
             b.bundle(function (err, bundledContents) {
+                if (err) {
+                    console.log('BROWSERIFY BUNDLING ERROR!', err.message, Object.keys(err));
+
+                    // if browserify provided filepath/linenumber details, try to read the file
+                    var relativePath;
+                    var gotFile = new Promise(function (resolve) {
+                        if (!err.filename) return resolve(false);
+
+                        relativePath = path.relative(self.sourceDir, err.filename);
+                        // console.log('LOADING', relativePath);
+
+                        // we have to read the file again :(
+                        self.readFile(relativePath, function (readErr, contents) {
+                            if (readErr || (contents === false)) {
+                                console.log('readErr', readErr);
+                                return resolve(false);
+                            }
+
+                            return resolve(contents);
+                        });
+                    });
+
+                    gotFile.then(function (contents) {
+                        // log full details
+                        if (contents) {
+                            self.report('exhibit-browserify', err.message.split(' while parsing file:')[0], {
+                                file: {
+                                    path: relativePath,
+                                    contents: contents
+                                },
+                                line: err.line,
+                                column: err.column
+                            });
+                        }
+                        else {
+                            // just log the message, no file details
+                            self.report('exhibit-browserify', err.message);
+                        }
+                        done();
+                    });
+
+                    return;
+                }
+
                 var dotIndex = file.path.lastIndexOf('.'),
                     slashIndex = file.path.lastIndexOf('/');
                 if (dotIndex > slashIndex || slashIndex === -1) {
                     file.path = file.path.substring(0, dotIndex + 1) + 'js';
-                }
-
-                if (err) {
-                    console.log('BROWSERIFY BUNDLING ERROR!', err.stack);
-
-                    done({
-                        message: err.message,
-                        file: path.join(self.sourceDir, file.path),
-                        line: err.line, // never seems to work; find out how to get this properly
-                        column: err.column // ditto
-                    });
-                    return;
                 }
 
                 // update the output file with the new string, then pass it on
